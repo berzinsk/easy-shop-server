b812d65c3806aca14d0bfe1f833076c9467eb49f
Karlis Berzins <berzinsh.karlis@gmail.com>
2021-02-19 06:59:40 +0200

    Add dotenv library to allow reading environment variables

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
+require('dotenv/config')
+
 const express = require('express')
 const app = express()
 
 const port = 3000
 
-app.get('/', (req, res) => {
+const api = process.env.API_URL
+
+app.get(`${api}/products`, (req, res) => {
   res.send('hello API')
 })
 
 app.listen(port, () => {
+  console.log(api)
   console.log(`Server is running http://localhost:${port}`)
 })
