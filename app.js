6fb9ad1e4b5514ff758f516d5ce93d901705f84b
Karlis Berzins <berzinsh.karlis@gmail.com>
2021-02-19 06:52:49 +0200

    Create simple express server

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log('hello world')
+const express = require('express')
+const app = express()
+
+const port = 3000
+
+app.listen(port, () => {
+  console.log(`Server is running http://localhost:${port}`)
+})
