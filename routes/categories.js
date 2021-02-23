23196bff502661da432572f58c30df2e5baf2fcc
Karlis Berzins <berzinsh.karlis@gmail.com>
2021-02-23 06:22:43 +0200

    Add API to POST and DELETE categories

--- routes/categories.js
+++ routes/categories.js
@@ -2,13 +2,43 @@ const {Category} = require('../models/category');
 const express = require('express');
 const router = express.Router();
 
-router.get(`/`, async (req, res) =>{
-    const categoryList = await Category.find();
+router.get('/', async (req, res) =>{
+  const categoryList = await Category.find();
 
-    if(!categoryList) {
-        res.status(500).json({success: false})
-    } 
-    res.send(categoryList);
+  if (!categoryList) {
+    res.status(500).json({ success: false })
+  }
+
+  res.send(categoryList);
 })
 
-module.exports =router;
+router.post('/', async (req, res) => {
+  let category = new Category({
+    name: req.body.name,
+    icon: req.body.icon,
+    color: req.body.color,
+  })
+
+  try {
+    category = await category.save()
+    res.send(category)
+  } catch {
+    res.status(404).send('the category cannot be created!')
+  }
+})
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const category = await Category.findByIdAndRemove(req.params.id)
+
+    if (category) {
+      res.status(200).json({ success: true, message: 'The category is deleted.' })
+    } else {
+      res.status(404).json({ success: false, message: 'Category not found' })
+    }
+  } catch (error) {
+    res.status(400).json({ success: false, error })
+  }
+})
+
+module.exports = router;
