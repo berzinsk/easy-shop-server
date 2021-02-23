5e506af4ad5046db3ca6f0463893be5f7e9ed7bb
Karlis Berzins <berzinsh.karlis@gmail.com>
2021-02-23 07:15:01 +0200

    Update POST API to store product

--- routes/products.js
+++ routes/products.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 const { Product } = require('../models/product')
+const { Category } = require('../models/category')
 
 router.get(`/`, async (req, res) => {
   try {
@@ -14,16 +15,30 @@ router.get(`/`, async (req, res) => {
 
 router.post(`/`, async (req, res) => {
   try {
+    const category = await Category.findById(req.body.category)
+
+    if (!category) {
+      return res.status(400).json({ success: false, error: 'Category not found!' })
+    }
+
     const product = new Product({
       name: req.body.name,
+      description: req.body.description,
+      richDescription: req.body.richDescription,
       image: req.body.image,
-      countInStock: req.body.countInStock
+      brand: req.body.brand,
+      price: req.body.price,
+      category: req.body.category,
+      countInStock: req.body.countInStock,
+      rating: req.body.rating,
+      numReviews: req.body.numReviews,
+      isFeatured: req.body.isFeatured,
     })
 
     const createdProduct = await product.save()
     res.status(201).json(createdProduct)
   } catch (error) {
-    res.status(500).json({ error, success: false })
+    res.status(500).json({ success: false, error })
   }
 })
 
