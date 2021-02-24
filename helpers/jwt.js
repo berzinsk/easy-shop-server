4399bf09ae433161119f496b5322b030639a43ad
Karlis Berzins <berzinsh.karlis@gmail.com>
2021-02-24 17:23:56 +0200

    Add public products API to get excluded from jwt

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -2,14 +2,27 @@ const expressJwt = require('express-jwt')
 
 function authJwt() {
   const secret = process.env.SECRET
+  const api = process.env.API_URL
 
   return expressJwt({
     secret,
     algorithms: ['HS256'],
   }).unless({
     path: [
-      '/api/v1/users/login',
-      '/api/v1/users/register',
+      {
+        url: `${api}/users/login`,
+      },
+      {
+        url: `${api}/users/register`,
+      },
+      {
+        url: /\/api\/v1\/products(.*)/,
+        methods: ['GET', 'OPTIONS']
+      },
+      {
+        url: /\/api\/v1\/categories(.*)/,
+        methods: ['GET', 'OPTIONS']
+      },
     ],
   })
 }
