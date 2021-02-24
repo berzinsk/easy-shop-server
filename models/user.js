92dcda4efee374f1abba8ed9f99d3d568685c19c
Karlis Berzins <berzinsh.karlis@gmail.com>
2021-02-24 07:07:20 +0200

    Add simple POST API to create users

--- models/user.js
+++ models/user.js
@@ -29,7 +29,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     default: '',
   },
-  zip :{
+  zip: {
     type: String,
     default: '',
   },
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,8 @@
-const {User} = require('../models/user');
+const { User } = require('../models/user');
 const express = require('express');
 const router = express.Router();
 
-router.get(`/`, async (req, res) =>{
+router.get('/', async (req, res) =>{
   const userList = await User.find();
 
   if(!userList) {
@@ -10,6 +10,33 @@ router.get(`/`, async (req, res) =>{
   }
 
   res.send(userList);
+})
+
+router.post('/', async (req, res) => {
+  let user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    passwordHash: req.body.passwordHash,
+    phone: req.body.phone,
+    isAdmin: req.body.isAdmin,
+    street: req.body.street,
+    apartment: req.body.apartment,
+    zip: req.body.zip,
+    city: req.body.city,
+    country: req.body.country,
+  })
+
+  try {
+    user = await user.save()
+
+    if (user) {
+      res.send(user)
+    } else {
+      res.status(400).json({ success: false, error: 'The user cannot be created!' })
+    }
+  } catch (error) {
+    res.status(400).json({ success: false, error })
+  }
 })
 
 module.exports =router;
