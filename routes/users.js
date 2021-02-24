8f6a344599e8094fb954a099d41c120c4d15c8ca
Karlis Berzins <berzinsh.karlis@gmail.com>
2021-02-24 07:10:44 +0200

    Encrypt user password using bcrypt

--- routes/users.js
+++ routes/users.js
@@ -1,22 +1,24 @@
-const { User } = require('../models/user');
-const express = require('express');
-const router = express.Router();
+const { User } = require('../models/user')
+const express = require('express')
+const bcrypt = require('bcryptjs')
+
+const router = express.Router()
 
 router.get('/', async (req, res) =>{
-  const userList = await User.find();
+  const userList = await User.find()
 
   if(!userList) {
     res.status(500).json({success: false})
   }
 
-  res.send(userList);
+  res.send(userList)
 })
 
 router.post('/', async (req, res) => {
   let user = new User({
     name: req.body.name,
     email: req.body.email,
-    passwordHash: req.body.passwordHash,
+    passwordHash: bcrypt.hashSync(req.body.password, 10),
     phone: req.body.phone,
     isAdmin: req.body.isAdmin,
     street: req.body.street,
