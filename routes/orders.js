9f8b4bbb05c9e27e3d624e65c2ff42ec56519761
Karlis Berzins <berzinsh.karlis@gmail.com>
2021-02-25 06:44:47 +0200

    Create POST API to allow creating orders

--- routes/orders.js
+++ routes/orders.js
@@ -1,6 +1,8 @@
-const {Order} = require('../models/order')
 const express = require('express')
 const router = express.Router()
+
+const { Order } = require('../models/order')
+const { OrderItem } = require('../models/order-item')
 
 router.get(`/`, async (req, res) => {
   try {
@@ -11,6 +13,45 @@ router.get(`/`, async (req, res) => {
     }
 
     res.send(orderList)
+  } catch (error) {
+    res.send(error)
+  }
+})
+
+router.post('/', async (req, res) => {
+  try {
+    const orderItemIds = await Promise.all(req.body.orderItems.map(async orderItem => {
+      let newOrderItem = new OrderItem({
+        quantity: orderItem.quantity,
+        product: orderItem.product,
+      })
+
+      newOrderItem = await newOrderItem.save()
+
+      return newOrderItem.id
+    }))
+
+    let order = new Order({
+      orderItems: orderItemIds,
+      shippingAddress1: req.body.shippingAddress1,
+      shippingAddress2: req.body.shippingAddress2,
+      city: req.body.city,
+      zip: req.body.zip,
+      country: req.body.country,
+      phone: req.body.phone,
+      status: req.body.status,
+      totalPrice: req.body.totalPrice,
+      user: req.body.user,
+      dateOrdered: req.body.dateOrdered,
+    })
+
+    order = await order.save()
+
+    if (!order) {
+      return res.status(404).json({ success: false, error: 'Order cannot be created!' })
+    }
+
+    res.send(order)
   } catch (error) {
     res.send(error)
   }
