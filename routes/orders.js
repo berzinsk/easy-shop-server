69f4fbfc268aa2c71348f414b841c45a6a2da70b
Karlis Berzins <berzinsh.karlis@gmail.com>
2021-02-25 07:04:12 +0200

    Add api to allow updating order status

--- routes/orders.js
+++ routes/orders.js
@@ -77,4 +77,20 @@ router.post('/', async (req, res) => {
   }
 })
 
+router.put('/:id', async (req, res) => {
+  try {
+    const order = await Order.findByIdAndUpdate(req.params.id, {
+      status: req.body.status,
+    }, { new: true })
+
+    if (order) {
+      res.status(200).send(order)
+    } else {
+      res.status(404).json({ success: false, message: 'Order can\'t be updated.' })
+    }
+  } catch (error) {
+    res.status(400).send(error)
+  }
+})
+
 module.exports =router
